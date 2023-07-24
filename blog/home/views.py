23f12d1c94e633b652c3edfe5f26db3c37177a95
Amircroo7 <a.a.amirhasan1921@gmail.com>
2023-07-24 18:09:11 +0330

    Register Form

--- blog/home/views.py
+++ blog/home/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View 
 
-# Create your views here.
-def home(request):
-    return HttpResponse('home')
+class HomeView(View):
+    def get(self, request):
+        return render(request, 'home/index.html')
