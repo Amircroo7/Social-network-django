e5ace82ba040106e4aae87e35a6c8b5024242209
Amircroo7 <a.a.amirhasan1921@gmail.com>
2023-08-01 15:01:08 +0330

    Update url and View

--- blog/home/views.py
+++ blog/home/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.views import View
 from .models import Post
+from .forms import PostCreateUpdateForm
+from django.utils.text import slugify
 
 
 class HomeView(View):
@@ -27,3 +29,32 @@ class PostDeleteView(LoginRequiredMixin, View):
         else:
             messages.error(request, 'you cant delete this post')
         return redirect('home:home')
+
+
+class PostUpdateView(LoginRequiredMixin, View):
+    form_class = PostCreateUpdateForm
+
+    def setup(self, request, *args, **kwargs):
+        self.post_instance = Post.objects.get(pk=kwargs['post_id'])
+
+    def dispatch(self, request, *args, **kwargs):
+        post = self.post_instance
+        if not post.user.id == request.user.id:
+            messages.error(request, 'you cant update this post', 'danger')
+            return redirect('home:home')
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        post = self.post_instance
+        form = self.form_class(instance=post)
+        return render(request, 'home/update.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        post = self.post_instance
+        form = self.form_class(request.Post, instance=post)
+        if form.is_valid():
+            new_post = form.save(commit=False)
+            new_post.slug = slugify(form.cleaned_data['body'][:30])
+            new_post.save()
+            messages.success(request, 'you updated this post', 'success')
+            return redirect('home:post_detail', post.id, post.slug)
