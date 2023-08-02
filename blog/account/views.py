7310961b082fe8d85007f231da3e2d663eb03899
Amircroo7 <a.a.amirhasan1921@gmail.com>
2023-08-02 15:19:45 +0330

    Create comment model and view

--- blog/account/views.py
+++ blog/account/views.py
@@ -35,10 +35,10 @@ class UserLoginView(View):
     form_class = UserLoginForm
     template_name = 'account/login.html'
 
-    # def setup(self, request, *args, **kwargs):
-    #     self.next = request.GET.get('next')
-    #     return super().setup(request, *args, **kwargs)
-    #
+    def setup(self, request, *args, **kwargs):
+        self.next = request.GET.get('next')
+        return super().setup(request, *args, **kwargs)
+
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated:
             return redirect('home:home')
--- blog/home/views.py
+++ blog/home/views.py
@@ -3,9 +3,11 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
-from .models import Post
-from .forms import PostCreateUpdateForm
+from .models import Post, Comment
+from .forms import PostCreateUpdateForm, CommentCreateForm
 from django.utils.text import slugify
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 
 class HomeView(View):
@@ -15,9 +17,27 @@ class HomeView(View):
 
 
 class PostDetailView(View):
-    def get(self, request, post_id, slug_id):
-        post = get_object_or_404(Post, pk=post_id, slug=slug_id)
-        return render(request, 'home/detail.html', {'post': post})
+    form_class = CommentCreateForm
+
+    def setup(self, request, *args, **kwargs):
+        self.post_instance = get_object_or_404(Post, pk=kwargs['post_id'], slug=kwargs['slug_id'])
+        return super(Post, self).setup(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        comments = self.post_instance.pcomments.filter(is_reply=False)
+        return render(request, 'home/detail.html',
+                      {'post': self.post_instance, 'comments': comments, 'form': self.form_class})
+
+    @method_decorator(login_required)
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            new_comment.user = request.user
+            new_comment.post = self.post_instance
+            new_comment.save()
+            messages.success(request, 'your comment submitted', 'success')
+            return redirect('home:posr_detail', self.post_instance.id, self.post_instance.slug)
 
 
 class PostDeleteView(LoginRequiredMixin, View):
@@ -62,6 +82,7 @@ class PostUpdateView(LoginRequiredMixin, View):
 
 class PostCreateView(LoginRequiredMixin, View):
     form_class = PostCreateUpdateForm
+
     def get(self, request, *args, **kwargs):
         form = self.form_class()
         return render(request, 'home/create.html', {'form': form})
